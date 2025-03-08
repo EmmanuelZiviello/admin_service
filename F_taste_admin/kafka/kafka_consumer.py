3d7c32e0971e634c88d09b32530c2060c951d54a
EmmanuelZiviello <124155626+EmmanuelZiviello@users.noreply.github.com>
2025-03-08 23:53:09 +0100

    spostato consumer_response nel file utils

--- F_taste_admin/kafka/kafka_consumer.py
+++ F_taste_admin/kafka/kafka_consumer.py
@@ -26,26 +26,7 @@ consumer = KafkaConsumer(
     value_deserializer=lambda x: json.loads(x.decode("utf-8"))
 )
 
-#consumer dedicato a ricevere solo risposte da altri servizi
-consumer_response = KafkaConsumer(
-    'patient.delete.success',
-    'patient.delete.failed',
-    'patient.getAll.success',
-    'patient.getAll.failed',
-    'dietitian.delete.success',
-    'dietitian.delete.failed',
-    'dietitian.getAll.success',
-    'dietitian.getAll.failed',
-    bootstrap_servers=KAFKA_BROKER_URL,
-    client_id="admin_consumer",
-    group_id="admin_service_response",
-    security_protocol="SSL",
-    ssl_cafile=os.path.join(CERTS_DIR, "ca.pem"),  # Percorso del certificato CA
-    ssl_certfile=os.path.join(CERTS_DIR, "service.cert"),  # Percorso del certificato client
-    ssl_keyfile=os.path.join(CERTS_DIR, "service.key"),  # Percorso della chiave privata
-    auto_offset_reset="earliest",
-    value_deserializer=lambda x: json.loads(x.decode("utf-8"))
-)
+
 
 
 
